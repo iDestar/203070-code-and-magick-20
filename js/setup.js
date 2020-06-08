38c88b279574b0e9a2c22bf3bdabde1b9177a8f6
iDestar <destarpwnz@gmai.com>
2020-06-08 18:40:15 +0300

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,25 +1,25 @@
 'use strict';
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURMANE = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARD_EYES_COLOR = ['rgb(0,0,0)', 'rgb(255,0,0)', '(0,0,255)', 'rgb(255,255,0)', 'rgb(0,128,0)'];
+var WIZARD_SURMANES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLORS = ['rgb(0,0,0)', 'rgb(255,0,0)', '(0,0,255)', 'rgb(255,255,0)', 'rgb(0,128,0)'];
 
 var getWizardName = function () {
   var nameRandom = Math.floor(Math.random() * WIZARD_NAMES.length);
 
-  return (WIZARD_NAMES[nameRandom]) + ' ' + (WIZARD_SURMANE[nameRandom]);
+  return (WIZARD_NAMES[nameRandom]) + ' ' + (WIZARD_SURMANES[nameRandom]);
 };
 
 var getWizardColorCoat = function () {
-  var coatRandom = Math.floor(Math.random() * WIZARD_COAT_COLOR.length);
+  var coatRandom = Math.floor(Math.random() * WIZARD_COAT_COLORS.length);
 
-  return (WIZARD_COAT_COLOR[coatRandom]);
+  return (WIZARD_COAT_COLORS[coatRandom]);
 };
 
 var getWizadEyesColor = function () {
-  var eyesRandom = Math.floor(Math.random() * WIZARD_EYES_COLOR.length);
+  var eyesRandom = Math.floor(Math.random() * WIZARD_EYES_COLORS.length);
 
-  return (WIZARD_EYES_COLOR[eyesRandom]);
+  return (WIZARD_EYES_COLORS[eyesRandom]);
 };
 
 var userDialog = document.querySelector('.setup');
