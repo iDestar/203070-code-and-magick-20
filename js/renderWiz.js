4340174b4f75496a7db90157877cff1b93046a16
iDestar <destarpwnz@gmai.com>
2020-07-15 20:55:50 +0300

    6 - 1

--- js/renderWiz.js
+++ js/renderWiz.js
@@ -2,50 +2,55 @@
 'use strict';
 
 (function () {
-  var userDialog = document.querySelector('.setup');
-  var similarListElement = userDialog.querySelector('.setup-similar-list');
-  userDialog.querySelector('.setup-similar').classList.remove('hidden');
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-.content
-.querySelector('.setup-similar-item');
+  var MAX_SIMILAR_WIZARD_COUNT = 4;
 
-  var wizards = [
-    {
-      name: window.data.getWizardName(),
-      coatColor: window.data.getWizardColorCoat(),
-      eyesColor: window.data.getWizadEyesColor()
-    },
-    {
-      name: window.data.getWizardName(),
-      coatColor: window.data.getWizardColorCoat(),
-      eyesColor: window.data.getWizadEyesColor()
-    },
-    {
-      name: window.data.getWizardName(),
-      coatColor: window.data.getWizardColorCoat(),
-      eyesColor: window.data.getWizadEyesColor()
-    },
-    {
-      name: window.data.getWizardName(),
-      coatColor: window.data.getWizardColorCoat(),
-      eyesColor: window.data.getWizadEyesColor()
-    }
-  ];
+  var userDialog = document.querySelector('.setup');
+
+  var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
 
     return wizardElement;
   };
 
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < wizards.length; i++) {
-    fragment.appendChild(renderWizard(wizards[i]));
-  }
-  similarListElement.appendChild(fragment);
+  var successHandler = function (wizards) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < MAX_SIMILAR_WIZARD_COUNT; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+
+    userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.load(successHandler, errorHandler);
+
+
+  var form = userDialog.querySelector('.setup-wizard-form');
+  form.addEventListener('submit', function (evt) {
+    window.upload(new FormData(form), function (response) {
+      userDialog.classList.add('hidden');
+    });
+    evt.preventDefault();
+  });
 
 })();
